5414dc65bc739f87c3a9da5cd0185134e6ba1cb5
boromeot <boromeotyler@gmail.com>
2022-10-12 16:59:12 -0700

    added first test post request

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ if (process.env.NODE_ENV !== 'production') {
 };
 
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const UserModel = require('./models/Users');
-const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.SECRET);
 // First api endpoint
@@ -17,6 +19,14 @@ app.get("/Users", (req, res) => {
             res.json(result);
         }
     });
+});
+
+app.post("/User", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
 });
 
 app.listen(3001, () => {
