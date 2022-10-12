ab2b8216a8a89337980d6d6954fc7a9a2a532230
boromeot <boromeotyler@gmail.com>
2022-10-12 15:52:49 -0700

    added dot env package for environment variables

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+};
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect(process.env.SECRET);
 app.listen(3001, () => {
     console.log('server running!');
 });
