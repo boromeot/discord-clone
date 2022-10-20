ccb473ae29630caecbda95ffd79ba57e92ab295c
boromeot <boromeotyler@gmail.com>
2022-10-20 12:13:57 -0700

    refactored register page html/css

--- frontend/src/Register/RegisterPage.js
+++ frontend/src/Register/RegisterPage.js
@@ -5,18 +5,18 @@ const RegisterPage = () => {
     <div className="regsiterpage">
       <form className="register">
         <h1 className="register-header">Create an account</h1>
-        <section className="register-form">
+        <section className="register-main">
           <div className="register-mb20">
-            <label>Email</label>
-            <input></input>
+            <label className="register-label">Email</label>
+            <input className="register-input" />
           </div>
           <div className="register-mb20">
-            <label>Username</label>
-            <input></input>
+            <label className="register-label">Username</label>
+            <input className="register-input" />
           </div>
           <div className="register-mb20">
-            <label>Password</label>
-            <input></input>
+            <label className="register-label">Password</label>
+            <input className="register-input" />
           </div>
         </section>
       </form>
