b75d12d4e2464a19bf635764add7dd3df824f324
boromeot <boromeotyler@gmail.com>
2022-10-28 12:32:16 -0700

    added onclick actions to options

--- frontend/src/Register/RegisterPage.js
+++ frontend/src/Register/RegisterPage.js
@@ -17,23 +17,17 @@ for (let i = 1; i <= 31; i++) {
 
 const YEARS = [];
 
-for (let i = 1870; i <= 2022; i++) {
+for (let i = 2019; i >= 1870; i--) {
   YEARS.push(i);
 }
 
-const DropDownOption = ({ name }) => {
-  return (
-    <div className="dropdown-option">{ name }</div>
-  )
-}
-
-const Select = ({ className="", options=[] }) => {
+const Select = ({ className="", options=[], input, setInput }) => {
   return (
       <div className={className}>
         <div className="select-option">
           <div className="select-textWrapper">
-            <div className="select-text"></div>
-            <input className="select-input"/>
+            <div className="select-text">{ input }</div>
+            <input className="select-input" onChange={e => {setInput(e.target.value)}}/>
           </div>
           <div className="select-chevron">
             <Chevron />
@@ -42,7 +36,7 @@ const Select = ({ className="", options=[] }) => {
         <div className="dropdown">
           {options.map(option => {
             return (
-              <DropDownOption name={option} />
+              <div className="dropdown-option" onClick={() => setInput(option)}>{option}</div>
             )
           })}
         </div>
@@ -54,6 +48,9 @@ const RegisterPage = () => {
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  const [year, setYear] = useState('');
 
   return (
     <div className="regsiterpage">
@@ -75,9 +72,9 @@ const RegisterPage = () => {
           <fieldset> 
             <legend className="register-label">Date of birth</legend>
             <div className="register-dropdowns">
-              <Select className="month" options={MONTHS} />
-              <Select className="day" options={DAYS} />
-              <Select className="year" options={YEARS} />
+              <Select className="month" options={MONTHS} input={month} setInput={setMonth} />
+              <Select className="day" options={DAYS} input={day} setInput={setDay} />
+              <Select className="year" options={YEARS} input={year} setInput={setYear} />
             </div>
           </fieldset>
           <div className="margintop20">
